00849b12662a6ebe8dd45d7827583350ce2770d0
Helen <helendianaf@gmail.com>
2024-01-04 18:27:55 -0300

    exericio modulo 26

---
+++ typescript/exercicio_typescript.ts
@@ -0,0 +1,15 @@
+//1 - soma
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+const resultadoMultiplicacao: number = multiplicacao(3, 4);
+console.log(resultadoMultiplicacao);
+
+//2 - saudacao
+function saudacao(nome: string): string {
+    return `Olá ${nome}!`;
+}
+
+const mensagemSaudacao: string = saudacao("Helen");
+console.log(mensagemSaudacao); 
